f7e8a8d9cda949fdb2dbd2ba028a94b5ac6723c2
tourze-ci <you@example.com>
2025-04-14 09:47:20 +0000

    feat(json-rpc-turbo-bundle): 添加 JSON-RPC 请求处理服务及依赖

---
+++ src/Service/JsonRpcRequestHandler.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Tourze\JsonRPCTurboBundle\Service;
+
+use Carbon\Carbon;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Tourze\JsonRPC\Core\Domain\JsonRpcMethodInterface;
+use Tourze\JsonRPC\Core\Event\AbstractOnMethodEvent;
+use Tourze\JsonRPC\Core\Event\MethodExecuteFailureEvent;
+use Tourze\JsonRPC\Core\Event\MethodExecuteSuccessEvent;
+use Tourze\JsonRPC\Core\Exception\JsonRpcInvalidParamsException;
+use Tourze\JsonRPC\Core\Exception\JsonRpcMethodNotFoundException;
+use Tourze\JsonRPC\Core\Model\JsonRpcRequest;
+use Tourze\JsonRPC\Core\Model\JsonRpcResponse;
+
+/**
+ * Class JsonRpcRequestHandler
+ */
+class JsonRpcRequestHandler
+{
+    public function __construct(
+        private readonly MethodResolver $methodResolver,
+        private readonly ResponseCreator $responseCreator,
+        private readonly EventDispatcherInterface $eventDispatcher,
+        private readonly JsonRpcParamsValidator $methodParamsValidator,
+    ) {
+    }
+
+    /**
+     * @throws JsonRpcInvalidParamsException
+     * @throws JsonRpcMethodNotFoundException
+     */
+    public function processJsonRpcRequest(JsonRpcRequest $request): JsonRpcResponse
+    {
+        $method = $this->resolveMethod($request);
+
+        $this->validateParamList($request, $method);
+
+        $startTime = Carbon::now();
+        try {
+            $result = $method->__invoke($request);
+            $endTime = Carbon::now();
+
+            $event = new MethodExecuteSuccessEvent();
+            $event->setStartTime($startTime);
+            $event->setEndTime($endTime);
+            $event->setResult($result);
+            $event->setMethod($method);
+            $event->setJsonRpcRequest($request);
+            $this->eventDispatcher->dispatch($event);
+        } catch (\Throwable $exception) {
+            $endTime = Carbon::now();
+
+            $event = new MethodExecuteFailureEvent();
+            $event->setStartTime($startTime);
+            $event->setEndTime($endTime);
+            $event->setException($exception);
+            $event->setMethod($method);
+            $event->setJsonRpcRequest($request);
+            $this->eventDispatcher->dispatch($event);
+        }
+
+        return $this->createResponse($event);
+    }
+
+    /**
+     * @throws JsonRpcMethodNotFoundException
+     */
+    public function resolveMethod(JsonRpcRequest $request): JsonRpcMethodInterface
+    {
+        $method = $this->methodResolver->resolve($request->getMethod());
+
+        if (!$method instanceof JsonRpcMethodInterface) {
+            throw new JsonRpcMethodNotFoundException($request->getMethod(), [
+                'class' => $method ? $method::class : null,
+            ]);
+        }
+
+        return $method;
+    }
+
+    /**
+     * @throws JsonRpcInvalidParamsException
+     */
+    private function validateParamList(JsonRpcRequest $jsonRpcRequest, JsonRpcMethodInterface $method): void
+    {
+        if (null !== $this->methodParamsValidator) {
+            $violationList = $this->methodParamsValidator->validate($jsonRpcRequest, $method);
+
+            if ([] !== $violationList) {
+                throw new JsonRpcInvalidParamsException($violationList);
+            }
+        }
+    }
+
+    protected function createResponse(AbstractOnMethodEvent $event): JsonRpcResponse
+    {
+        if ($event instanceof MethodExecuteSuccessEvent) {
+            return $this->responseCreator->createResultResponse($event->getResult(), $event->getJsonRpcRequest());
+        }
+
+        /* @var $event MethodExecuteFailureEvent */
+        return $this->responseCreator->createErrorResponse(
+            $event->getException(),
+            $event->getJsonRpcRequest()
+        );
+    }
+}
