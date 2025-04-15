c453bd8bb30b24b91c90b74549296adf50543d96
tourze-ci <you@example.com>
2025-04-15 05:38:29 +0000

    refactor(bundles): 更新依赖

--- src/JsonRPCTurboBundle.php
+++ src/JsonRPCTurboBundle.php
@@ -3,7 +3,14 @@
 namespace Tourze\JsonRPCTurboBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BacktraceHelper\Backtrace;
+use Tourze\JsonRPCTurboBundle\Service\JsonRpcRequestHandler;
 
 class JsonRPCTurboBundle extends Bundle
 {
+    public function boot(): void
+    {
+        parent::boot();
+        Backtrace::addProdIgnoreFiles((new \ReflectionClass(JsonRpcRequestHandler::class))->getFileName());
+    }
 }
